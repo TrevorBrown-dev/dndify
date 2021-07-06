b197279f2383a098feb8f0a8747bc412d3a2d7f3
Trevor Brown <trevorbrown-dev@github.com>
2021-07-06 13:38:40 -0400

    Adding item form

--- src/components/CreateCharacter/Items/Items.tsx
+++ src/components/CreateCharacter/Items/Items.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { CharacterProps, Section } from "..";
-import { ItemType } from "../../../models/items";
-import { ItemCard } from "./ItemCard";
-
+import React from 'react';
+import { CharacterProps, Section } from '..';
+import { ItemType } from '../../../models/items';
+import { ItemCard } from './ItemCard';
 
 /*
     The item card should take in an item object and render its details.
@@ -10,61 +9,63 @@ import { ItemCard } from "./ItemCard";
     It should be displayed in a tiled-grid form similar to a equipment card
 */
 
-
-
 const mapItemTypes = (): JSX.Element[] => {
     const els: JSX.Element[] = [];
     const vals = Object.values(ItemType);
 
     for (const key of vals) {
-        els.push(<option key={key} defaultValue={key}>
-            {key}
-        </option>)
+        els.push(
+            <option key={key} defaultValue={key}>
+                {key}
+            </option>
+        );
     }
     return els;
-}
+};
 
 const SelectItemType: React.FC = () => {
     return (
-        <div className="center-grid-justified" style={{columnGap:"0.5em"}}>
-            <div className="left">
-                
-                <label htmlFor="item-type">Item Type: </label>
-            </div>
-            <div className="right">
-
-                <select name="item-type" id="item-type">
-                <option selected defaultValue="" style={{color: "darkgray"}}>Chose a Type...</option>
-                    {mapItemTypes()}
-                </select>
-            </div>
-        </div>
-)
-}
-
-const InputName: React.FC = () => {
-    return (
-        <div className="center-grid-justified">
-            <div className="left">
-
-            <label htmlFor="item-name">Name: </label>
-
-            </div>
-            <div className="right"><input type="text" id="item-name" /></div>
-        </div>
+        <select
+            name="item-type"
+            id="item-type"
+            style={{ width: '100%', height: '100%' }}>
+            <option selected defaultValue="" style={{ color: 'darkgray' }}>
+                Chose a Type...
+            </option>
+            {mapItemTypes()}
+        </select>
     );
-}
-
+};
 
 export const Items: React.FC<CharacterProps> = ({ character }) => {
-
     return (
-        <Section header="Items" id="items" title="Items" wikiReference="https://www.dndbeyond.com/equipment" icon="fas fa-swords">
+        <Section
+            header="Items"
+            id="items"
+            title="Items"
+            wikiReference="https://www.dndbeyond.com/equipment"
+            icon="fas fa-swords">
             <div className="create-sec-items">
                 <div className="item-form">
-
-            <InputName />
-         <SelectItemType />
+                    <div id="name">
+                        <h3>
+                            <input
+                                type="text"
+                                id="item-name"
+                                placeholder="Name"
+                            />
+                        </h3>
+                    </div>
+                    <div id="rarity">rarity</div>
+                    <div id="type">
+                        <SelectItemType />
+                    </div>
+                    <div id="weight">weight</div>
+                    <div id="cost">cost</div>
+                    <div id="desc">description</div>
+                    <div id="submit">
+                        <button className="add-item">Add Item</button>
+                    </div>
                 </div>
                 <fieldset className="item-cards">
                     <legend>Items</legend>
@@ -74,7 +75,7 @@ export const Items: React.FC<CharacterProps> = ({ character }) => {
                     <ItemCard />
                     <ItemCard />
                 </fieldset>
-          </div>
+            </div>
         </Section>
     );
-}
+};
